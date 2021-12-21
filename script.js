7c2fcc20c075314b80539a8e5d1f4ba03b01aaa0
Md Shagor Hossain <79301601+shagor-coder@users.noreply.github.com>
2021-12-22 02:09:34 +0600

    New Update

--- script.js
+++
@@ -1,2 +0,0 @@
-$(".slider").slick({infinite:!0,slideToShow:1,slideToScroll:1});
-//# sourceMappingURL=src/css/script.js.map
--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,7 @@
-$(".slider").slick({infinite:!0,slideToShow:1,slideToScroll:1});
 //# sourceMappingURL=script.js.map
+const navBar = document.querySelector('.navbar')
+window.addEventListener('scroll', function () {
+  navBar.classList.toggle('active', scrollY > 50)
+})
+console.log(navBar)
+$('.slider').slick({ infinite: !0, slideToShow: 1, slideToScroll: 1 })
